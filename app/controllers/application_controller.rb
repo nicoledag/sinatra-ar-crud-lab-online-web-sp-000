eb7c3f0664a462a652953c640af9c655f2505c68
Nicole D <nnardella@gmail.com>
2019-02-05 01:08:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post '/articles' do
     Article.create(title: params[:title], content: params[:content])
-    @articles = Article.all
+    # @articles = Article.all
 
     erb :index
   end
