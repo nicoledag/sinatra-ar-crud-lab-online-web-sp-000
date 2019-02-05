4bc152704c814c71cb60be7c4911ca585e9d348b
Nicole D <nnardella@gmail.com>
2019-02-05 01:58:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,7 @@ class ApplicationController < Sinatra::Base
 
   patch '/articles/:id' do
     article = Article.find(params[:id])
-    article.update(title: params[:title], content: params[:content])
+    # article.update(title: params[:title], content: params[:content])
     binding.pry
   end
 
