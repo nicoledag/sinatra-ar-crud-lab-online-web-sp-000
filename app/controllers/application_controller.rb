7cd0fb6bece66218ca12c78aa35f7b958510bbf3
Nicole D <nnardella@gmail.com>
2019-02-05 01:56:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,6 +45,7 @@ class ApplicationController < Sinatra::Base
   patch '/articles/:id' do
     article = Article.find(params[:id])
     article.update(title: params[:title], content: params[:content])
+    article.save
   end
 
 
